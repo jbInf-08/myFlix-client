adb6094ea87230ee61bf715320c53b0855f92de3
Juan Bautista <144956427+jbInf-08@users.noreply.github.com>
2024-06-13 13:01:52 -0500

    updated movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,27 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types'; // Ensure PropTypes is imported
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-  return (
-      <Card>
-          <Card.Img variant="top" src={movie.imageUrl} />
-          <Card.Body>
-              <Card.Title>{movie.title}</Card.Title>
-              <Card.Text>{movie.director.Name}</Card.Text>
-              <Button
-                  onClick={() => {
-                      onMovieClick(movie)
-                  }} variant="link">
-                  Open
-              </Button>
-          </Card.Body>
-      </Card>
-  );
-};
-MovieCard.propTypes = {
-  movie: PropType.shape({
-      title: PropType.string.isRequired,
-      imageUrl: PropType.string.isRequired,
-  }).isRequired
-};
+    return (
+      <div
+        onClick={() => {
+          onMovieClick(movie);
+        }}
+      >
+        {movie.Title}
+      </div>
+    );
+  };
+  MovieCard.propTypes = {
+    movie: PropType.shape({
+      Title: PropType.string.isRequired,
+      ImagePath: PropType.string.isRequired, 
+    }).isRequired
+  };
 
 export default MovieCard;
